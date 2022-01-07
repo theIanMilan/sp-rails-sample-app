8e92fb66f228d88b12490aaa8a39e79bcdd025cf
theIanMilan <theIanMilan@users.noreply.github.com>
2022-01-07 21:13:39 +0800

    Add Session Model

---
+++ app/controllers/concerns/create_session.rb
@@ -0,0 +1,11 @@
+module CreateSession
+  extend ActiveSupport::Concern
+  require 'json_web_token'
+
+  def jwt_session_create(user_id)
+    user = User.find(user_id)
+    session = User.sessions.build
+
+    JsonWebToken.encode({ user_id: user_id, token: session.token }) if user && session.save
+  end
+end

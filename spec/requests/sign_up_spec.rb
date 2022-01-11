fe0c2b9a34f3d0d0636428f1955c10c65a51e7a5
theIanMilan <theIanMilan@users.noreply.github.com>
2022-01-11 15:05:49 +0800

    Add RSpec for Registrations#create

---
+++ spec/requests/sign_up_spec.rb
@@ -0,0 +1,55 @@
+require 'rails_helper'
+
+describe 'when signing up as a new user', type: :request do
+  before do
+    post '/api/v1/sign_up',
+         params: { registration: { username: 'HelloWorld', password: 'password1234', email: 'example@example.com',
+                                   first_name: 'Dee', last_name: 'Test' } }
+  end
+
+  context 'with valid params' do
+    it 'returns a valid JWT' do
+      expect(JSON.parse(response.body)['Authorization']).to include('Bearer')
+    end
+
+    it "returns the user's username" do
+      expect(JSON.parse(response.body)['user']['username']).to eq(controller.params[:registration][:username])
+    end
+
+    it "returns the user's bcrypt's encrypted password" do
+      expect(JSON.parse(response.body)['user']['password_digest']).to_not eq(nil)
+    end
+
+    it "returns the user's email" do
+      expect(JSON.parse(response.body)['user']['email']).to eq(controller.params[:registration][:email])
+    end
+
+    it "returns the user's first_name" do
+      expect(JSON.parse(response.body)['user']['first_name']).to eq(controller.params[:registration][:first_name])
+    end
+
+    it "returns the user's last_name" do
+      expect(JSON.parse(response.body)['user']['last_name']).to eq(controller.params[:registration][:last_name])
+    end
+
+    it 'returns a created status' do
+      expect(response).to have_http_status(:created)
+    end
+  end
+
+  # context 'with invalid params' do
+  #   before do
+  #     post '/api/v1/sign_up',
+  #          params: { registration: { username: 'HelloWorld', password: 'password1234', email: 1,
+  #                                    first_name: 'Dee', last_name: 'Test' } }
+  #   end
+
+  #   it 'returns a JSON with errors as a key' do
+  #     expect(JSON.parse(response.body)['errors']).to_not eq(nil)
+  #   end
+
+  #   it 'returns a status of unprocessable_entity' do
+  #     expect(response).to have_http_status(:unprocessable_entity)
+  #   end
+  # end
+end
